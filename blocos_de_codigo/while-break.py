50f827f950a74c91ce44f814a8aeeaffdb067b2f
SMARAPD - Bruno Empke <BEMPKE@SMARAPD.COM.BR>
2024-02-01 17:31:36 -0300

    Task: Atribuição Aritmética

--- blocos_de_codigo/while-break.py
+++ blocos_de_codigo/while-break.py
@@ -5,3 +5,22 @@ while (enquanto)
 Eexecuta uma ação enquanto uma condição for verdadeira.
 
 """
+
+contador = 0
+
+while contador <= 10:
+  print(contador)
+  contador = contador + 1
+
+
+
+# condicao_str = True
+
+# while condicao_str:
+#   nome = input("Digite seu nome: ")
+  
+#   if len(nome) >= 3:
+#     print(f"Seu nome é: {nome}")
+#     condicao_str = False
+#   else:
+#     print("Digite um nome válido")
